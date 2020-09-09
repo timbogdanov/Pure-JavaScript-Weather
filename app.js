0c837fc6459b446f3f14229990a7476774c855b2
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-09-08 17:55:54 -0700

    fix colors

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ window.addEventListener('load', () => {
   let weatherHumidity = document.querySelector('.humidity');
   let weatherWind = document.querySelector('.wind');
   let searchInput = document.querySelector('.search-input');
+  let weatherH1 = document.querySelector('.weather h1');
+  let detailH3 = document.querySelectorAll('.detail  h3');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -24,7 +26,6 @@ window.addEventListener('load', () => {
           return res.json();
         })
         .then((data) => {
-          console.log(data);
           const name = data.name;
           const description = data.weather[0].description;
           const { temp, humidity, feels_like } = data.main;
@@ -40,6 +41,48 @@ window.addEventListener('load', () => {
           weatherHumidity.textContent = humidity + '%';
           weatherWind.textContent = Math.round(wind) + 'mph';
           searchInput.value = name;
+
+          if (temp <= 49) {
+            weatherApp.style.background = '#3d405b';
+            tempDegree.style.color = '#FFFFFF';
+            tempDesc.style.color = '#FFFFFF';
+            locationName.style.color = '#FFFFFF';
+            weatherH1.style.color = '#FFFFFF';
+
+            detailH3.forEach((detail) => {
+              detail.style.color = '#3d405b';
+            });
+          } else if (temp >= 50 && temp <= 60) {
+            weatherApp.style.background = '#81b29a';
+            tempDegree.style.color = '#FFFFFF';
+            tempDesc.style.color = '#FFFFFF';
+            locationName.style.color = '#FFFFFF';
+            weatherH1.style.color = '#FFFFFF';
+
+            detailH3.forEach((detail) => {
+              detail.style.color = '#81b29a';
+            });
+          } else if (temp >= 60 && temp <= 74) {
+            weatherApp.style.background = '#f2cc8f';
+            tempDegree.style.color = '#FFFFFF';
+            tempDesc.style.color = '#FFFFFF';
+            locationName.style.color = '#FFFFFF';
+            weatherH1.style.color = '#FFFFFF';
+
+            detailH3.forEach((detail) => {
+              detail.style.color = '#f2cc8f';
+            });
+          } else if (temp >= 75) {
+            weatherApp.style.background = '#e07a5f';
+            tempDegree.style.color = '#FFFFFF';
+            tempDesc.style.color = '#FFFFFF';
+            locationName.style.color = '#FFFFFF';
+            weatherH1.style.color = '#FFFFFF';
+
+            detailH3.forEach((detail) => {
+              detail.style.color = '#e07a5f';
+            });
+          }
         });
 
       searchInput.addEventListener('keyup', function (event) {
@@ -57,8 +100,6 @@ window.addEventListener('load', () => {
               const { country } = data.sys;
               const wind = data.wind.speed;
 
-              // set DOM Elements from the API
-
               tempDegree.textContent = Math.round(temp) + '°';
               tempDesc.textContent = description;
               locationName.textContent = name + ', ' + country;
@@ -66,6 +107,48 @@ window.addEventListener('load', () => {
               weatherHumidity.textContent = humidity + '%';
               weatherWind.textContent = Math.round(wind) + 'mph';
               searchInput.value = name;
+
+              if (temp <= 49) {
+                weatherApp.style.background = '#3d405b';
+                tempDegree.style.color = '#FFFFFF';
+                tempDesc.style.color = '#FFFFFF';
+                locationName.style.color = '#FFFFFF';
+                weatherH1.style.color = '#FFFFFF';
+
+                detailH3.forEach((detail) => {
+                  detail.style.color = '#3d405b';
+                });
+              } else if (temp >= 50 && temp <= 60) {
+                weatherApp.style.background = '#81b29a';
+                tempDegree.style.color = '#FFFFFF';
+                tempDesc.style.color = '#FFFFFF';
+                locationName.style.color = '#FFFFFF';
+                weatherH1.style.color = '#FFFFFF';
+
+                detailH3.forEach((detail) => {
+                  detail.style.color = '#81b29a';
+                });
+              } else if (temp >= 60 && temp <= 74) {
+                weatherApp.style.background = '#f2cc8f';
+                tempDegree.style.color = '#FFFFFF';
+                tempDesc.style.color = '#FFFFFF';
+                locationName.style.color = '#FFFFFF';
+                weatherH1.style.color = '#FFFFFF';
+
+                detailH3.forEach((detail) => {
+                  detail.style.color = '#f2cc8f';
+                });
+              } else if (temp >= 75) {
+                weatherApp.style.background = '#e07a5f';
+                tempDegree.style.color = '#FFFFFF';
+                tempDesc.style.color = '#FFFFFF';
+                locationName.style.color = '#FFFFFF';
+                weatherH1.style.color = '#FFFFFF';
+
+                detailH3.forEach((detail) => {
+                  detail.style.color = '#e07a5f';
+                });
+              }
             });
         }
       });
