198dfb1327e075d4711678533535dda8c3262703
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-09-09 13:02:46 -0700

    add weather conditions icons

--- app.js
+++ app.js
@@ -36,27 +36,33 @@ window.addEventListener('load', () => {
           data.list.forEach((lapse) => {
             let newForecast = document.createElement('div');
             let newTime = document.createElement('div');
-            let newDesc = document.createElement('div');
             let newTemp = document.createElement('div');
-            newTime.classList.add('three-hour-time');
-            newDesc.classList.add('three-hour-desc');
-            newTemp.classList.add('three-hour-temp');
+            let weatherIcon = document.createElement('img');
+
             newForecast.classList.add('three-hour');
+            newTime.classList.add('three-hour-time');
+            newTemp.classList.add('three-hour-temp');
+            weatherIcon.classList.add('three-hour-icon');
+
             forecastWrapper.appendChild(newForecast);
             newForecast.appendChild(newTime);
-            newForecast.appendChild(newDesc);
             newForecast.appendChild(newTemp);
+            newForecast.appendChild(weatherIcon);
+
+            weatherIcon.src = `http://openweathermap.org/img/wn/${lapse.weather[0].icon}@2x.png`;
 
             let time = lapse.dt;
             let timeConvert = new Date(time * 1000);
-            let hours = timeConvert.getHours();
+            let hours = timeConvert.getHours() % 12;
             let minutes = '0' + timeConvert.getMinutes();
             let formattedTime = hours + ':' + minutes.substr(-2);
 
             newTime.textContent = formattedTime;
-            newDesc.textContent = lapse.weather[0].main;
             newTemp.textContent = Math.round(lapse.main.temp) + '°';
           });
+        })
+        .catch((error) => {
+          console.log(error.message);
         });
 
       fetch(api)
@@ -64,11 +70,18 @@ window.addEventListener('load', () => {
           return res.json();
         })
         .then((data) => {
+          console.log(data);
           const name = data.name;
           const description = data.weather[0].description;
           const { temp, temp_max, temp_min, humidity, feels_like } = data.main;
           const { country } = data.sys;
           const wind = data.wind.speed;
+
+          const currentTemp = document.querySelector('.current-temp');
+          let mainWeatherIcon = document.createElement('img');
+          mainWeatherIcon.classList.add('main-hour-icon');
+          currentTemp.prepend(mainWeatherIcon);
+          mainWeatherIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
           // set DOM Elements from the API
 
@@ -81,48 +94,6 @@ window.addEventListener('load', () => {
           weatherHigh.textContent = Math.round(temp_max) + '°';
           weatherLow.textContent = Math.round(temp_min) + '°';
           searchInput.value = name + ', ' + country;
-
-          if (temp <= 49) {
-            weatherApp.style.background = '#3d405b';
-            tempDegree.style.color = '#FFFFFF';
-            tempDesc.style.color = '#FFFFFF';
-            locationName.style.color = '#FFFFFF';
-            weatherH1.style.color = '#FFFFFF';
-
-            detailH3.forEach((detail) => {
-              detail.style.color = '#3d405b';
-            });
-          } else if (temp >= 50 && temp <= 60) {
-            weatherApp.style.background = '#81b29a';
-            tempDegree.style.color = '#FFFFFF';
-            tempDesc.style.color = '#FFFFFF';
-            locationName.style.color = '#FFFFFF';
-            weatherH1.style.color = '#FFFFFF';
-
-            detailH3.forEach((detail) => {
-              detail.style.color = '#81b29a';
-            });
-          } else if (temp >= 60 && temp <= 74) {
-            weatherApp.style.background = '#f2cc8f';
-            tempDegree.style.color = '#FFFFFF';
-            tempDesc.style.color = '#FFFFFF';
-            locationName.style.color = '#FFFFFF';
-            weatherH1.style.color = '#FFFFFF';
-
-            detailH3.forEach((detail) => {
-              detail.style.color = '#f2cc8f';
-            });
-          } else if (temp >= 75) {
-            weatherApp.style.background = '#e07a5f';
-            tempDegree.style.color = '#FFFFFF';
-            tempDesc.style.color = '#FFFFFF';
-            locationName.style.color = '#FFFFFF';
-            weatherH1.style.color = '#FFFFFF';
-
-            detailH3.forEach((detail) => {
-              detail.style.color = '#e07a5f';
-            });
-          }
         });
 
       searchInput.addEventListener('keyup', function (event) {
@@ -147,16 +118,18 @@ window.addEventListener('load', () => {
                 oldForecast.remove();
                 let newForecast = document.createElement('div');
                 let newTime = document.createElement('div');
-                let newDesc = document.createElement('div');
                 let newTemp = document.createElement('div');
+                let weatherIcon = document.createElement('img');
+
+                weatherIcon.classList.add('three-hour-icon');
                 newTime.classList.add('three-hour-time');
-                newDesc.classList.add('three-hour-desc');
                 newTemp.classList.add('three-hour-temp');
                 newForecast.classList.add('three-hour');
                 forecastWrapper.appendChild(newForecast);
                 newForecast.appendChild(newTime);
-                newForecast.appendChild(newDesc);
                 newForecast.appendChild(newTemp);
+                newForecast.appendChild(weatherIcon);
+                weatherIcon.src = `http://openweathermap.org/img/wn/${lapse.weather[0].icon}@2x.png`;
 
                 // let newTime = document.querySelector('.three-hour-time');
                 // let newDesc = document.querySelector('.three-hour-desc');
@@ -169,7 +142,6 @@ window.addEventListener('load', () => {
                 let formattedTime = hours + ':' + minutes.substr(-2);
 
                 newTime.textContent = formattedTime;
-                newDesc.textContent = lapse.weather[0].main;
                 newTemp.textContent = Math.round(lapse.main.temp) + '°';
               });
             });
@@ -200,48 +172,6 @@ window.addEventListener('load', () => {
               weatherHigh.textContent = Math.round(temp_max) + '°';
               weatherLow.textContent = Math.round(temp_min) + '°';
               searchInput.value = name + ', ' + country;
-
-              if (temp <= 49) {
-                weatherApp.style.background = '#3d405b';
-                tempDegree.style.color = '#FFFFFF';
-                tempDesc.style.color = '#FFFFFF';
-                locationName.style.color = '#FFFFFF';
-                weatherH1.style.color = '#FFFFFF';
-
-                detailH3.forEach((detail) => {
-                  detail.style.color = '#3d405b';
-                });
-              } else if (temp >= 50 && temp <= 60) {
-                weatherApp.style.background = '#81b29a';
-                tempDegree.style.color = '#FFFFFF';
-                tempDesc.style.color = '#FFFFFF';
-                locationName.style.color = '#FFFFFF';
-                weatherH1.style.color = '#FFFFFF';
-
-                detailH3.forEach((detail) => {
-                  detail.style.color = '#81b29a';
-                });
-              } else if (temp >= 60 && temp <= 74) {
-                weatherApp.style.background = '#f2cc8f';
-                tempDegree.style.color = '#FFFFFF';
-                tempDesc.style.color = '#FFFFFF';
-                locationName.style.color = '#FFFFFF';
-                weatherH1.style.color = '#FFFFFF';
-
-                detailH3.forEach((detail) => {
-                  detail.style.color = '#f2cc8f';
-                });
-              } else if (temp >= 75) {
-                weatherApp.style.background = '#e07a5f';
-                tempDegree.style.color = '#FFFFFF';
-                tempDesc.style.color = '#FFFFFF';
-                locationName.style.color = '#FFFFFF';
-                weatherH1.style.color = '#FFFFFF';
-
-                detailH3.forEach((detail) => {
-                  detail.style.color = '#e07a5f';
-                });
-              }
             });
         }
       });
